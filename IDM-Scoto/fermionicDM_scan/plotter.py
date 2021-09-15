3a8054d3039b46244148531ba3359bb77beafa34
humberto reyes <humberto@humbertos-MacBook-Pro.local>
2021-09-15 17:35:46 +0200

    fermionic DM results

--- IDM-Scoto/fermionicDM_scan/plotter.py
+++ IDM-Scoto/fermionicDM_scan/plotter.py
@@ -222,35 +222,36 @@ def plotCurvesTopo(frame,title,path_to_plot):
     
 
 def plotCurvesTopo2(frame,frameHSCP,title,path_to_plot):
-
-
+    
+    
+    #frame=frame.loc[frame['MASS37']<900]
     fig, ax = plt.subplots()
     #ax.scatter(frame['m(N1)'],frame['m(H+)'],c=frame['r_max'],alpha=1,cmap='jet',vmax=1.2,vmin=.1)
     #ax.set_xscale('log')
    # plt.colorbar()
     
-    ax.set_xlim(left=min(frame['m(H+)']),right=max(frame['m(H+)']))
-    ax.set_ylim(bottom=min(frame['m(N1)']),top=max(frame['m(N1)']))
+    ax.set_xlim(left=min(frame['MASS37']),right=max(frame['MASS37']))
+    ax.set_ylim(bottom=min(frame['MASS9000006']),top=max(frame['MASS9000006']))
     
-    plt.scatter(frame['m(H+)'],frame['m(N1)'],c=frame['r_max'],alpha=1,cmap='jet',vmax=1.2,vmin=.1)
+    plt.scatter(frame['MASS37'],frame['MASS9000006'],c=frame['r_max'],alpha=1,cmap='jet',vmax=1.2,vmin=.1,s=55)
     plt.colorbar(label='$r_{max}$')
     
     frame_displaced=frameHSCP.loc[frameHSCP['Disp']>1]
-    frame_analysis2d=frame_displaced[['m(H+)','m(N1)']]
+    frame_analysis2d=frame_displaced[['MASS37','MASS9000006']]
     frame_analysis2d=frame_analysis2d.to_numpy()
     print(frame_analysis2d)
-    alpha_shape = alphashape.alphashape(frame_analysis2d, .05)
+    alpha_shape = alphashape.alphashape(frame_analysis2d, .08)
     print(alpha_shape)
     ax.add_patch(PolygonPatch(alpha_shape, alpha=1,ec='pink',fill=False,zorder=20,lw=2,label='Displaced lepton'))
     
     
     
-    frame_excluded=frameHSCP.loc[frameHSCP['SModelS_status']=='Excluded']
-    frame_analysis=frame_excluded.loc[frame_excluded['Tx']!='TSmuSmuDisp']
-    frame_analysis2d=frame_analysis[['m(H+)','m(N1)']]
+    frame_excluded=frameHSCP.loc[frameHSCP['r_max']>1]
+    frame_analysis=frame_excluded.loc[frame_excluded['Tx']!='TSelSelDisp']
+    frame_analysis2d=frame_analysis[['MASS37','MASS9000006']]
     frame_analysis2d=frame_analysis2d.to_numpy()
     print(frame_analysis2d)
-    alpha_shape = alphashape.alphashape(frame_analysis2d, .05)
+    alpha_shape = alphashape.alphashape(frame_analysis2d, .09)
     print(alpha_shape)
     ax.add_patch(PolygonPatch(alpha_shape, alpha=1,ec='white',fill=False,lw=2,label='HSCP'))
     plt.legend(loc='upper left',framealpha=.6,labelcolor='black')
@@ -266,25 +267,133 @@ def plotCurvesTopo2(frame,frameHSCP,title,path_to_plot):
     return
  
     
-  
-    
+def plotCurvesTopoCtau2(frame,frameHSCP,title,path_to_plot):
 
+
+    #frame=frame.loc[frame['MASS37']<900]
+    frame=frame.loc[frame['ctau(37)']<10e5]
+    fig, ax = plt.subplots()
+    
+    
+    #ax.scatter(frame['m(N1)'],frame['m(H+)'],c=frame['r_max'],alpha=1,cmap='jet',vmax=1.2,vmin=.1)
+    #ax.set_xscale('log')
+   # plt.colorbar()
+    
+    ax.set_xlim(left=min(frame['MASS37']),right=max(frame['MASS37']))
+    ax.set_ylim(bottom=9e-3,top=1e3)
+    
+    plt.scatter(frame['MASS37'],frame['ctau(37)'],c=frame['r_max'],alpha=1,cmap='jet',vmax=1.2,vmin=.1,s=55)
+    plt.colorbar(label='$r_{max}$')
+    
+    frame_displaced=frameHSCP.loc[frameHSCP['Disp']>1]
+    frame_analysis2d=frame_displaced[['MASS37','ctau(37)']]
+    frame_analysis2d=frame_analysis2d.to_numpy()
+    print(frame_analysis2d)
+    alpha_shape = alphashape.alphashape(frame_analysis2d, .05)
+    print(alpha_shape)
+    ax.add_patch(PolygonPatch(alpha_shape, alpha=1,ec='pink',fill=False,zorder=20,lw=2,label='Displaced lepton'))
+    
+    
+    frameHSCP['ctau(37)']=frameHSCP['ctau(37)']
+    frame_excluded=frameHSCP.loc[frameHSCP['SModelS_status']=='Excluded']
+    print('heeelllooooo')
+    print(max(frame_excluded['ctau(37)']))
+    
+    frame_analysis=frame_excluded.loc[frame_excluded['ctau(37)']<10e3]
+    frame_analysis2d=frame_excluded[['MASS37','ctau(37)']]
+    frame_analysis2d=frame_analysis2d.to_numpy()
+    print(frame_analysis2d)
+    alpha_shape = alphashape.alphashape(frame_analysis2d, .002)
+    print(alpha_shape)
+    ax.add_patch(PolygonPatch(alpha_shape, alpha=1,ec='white',fill=False,lw=2,label='HSCP'))
+    plt.legend(loc='upper left',framealpha=.6,labelcolor='black')
+    #print(alpha_shape)
+   # Patch(alpha_shape,'.',c='yellow')
+    plt.yscale('log')
+    plt.ylabel('$ c\\tau_{H^{\\pm}}$ (m)',fontsize=12)
+    plt.xlabel('$ m_{H^{\\pm}}$ (GeV)',fontsize=12)
+    plt.title(title)
+    plt.savefig(path_to_plot)
+    plt.close()
    
+    return
+    
+
+def comparing_compression(frame_comp,frame_nocomp,title,path_to_plot):
+
+    r_ratio=frame_comp['r_max']/frame_nocomp['r_max']
+    #plt.scatter(frame_comp['MASS9000006'],frame_comp['MASS37'],c=r_ratio,alpha=1,cmap='jet',vmax=2,s=45)
+    print(min(r_ratio))
+    fig, ax = plt.subplots()
+    
+    ax.set_xlim(left=min(frame_comp['MASS37']),right=max(frame_comp['MASS37']))
+    ax.set_ylim(bottom=min(frame_comp['MASS9000006']),top=max(frame_comp['MASS9000006']))
+    
+    frame_comp_excluded=frame_comp.loc[frame_comp['SModelS_status']=='Excluded']
+    frame_comp_excluded=frame_comp_excluded[['MASS37','MASS9000006']]
+    frame_comp_excluded=frame_comp_excluded.to_numpy()
+    print(frame_comp_excluded)
+    alpha_shape = alphashape.alphashape(frame_comp_excluded, .099)
+    print(alpha_shape)
+    ax.add_patch(PolygonPatch(alpha_shape, alpha=1,ec='pink',fill=False,zorder=20,lw=2,label='True'))
+    
+    
+   
+    frame_nocomp_excluded=frame_nocomp.loc[frame_nocomp['r_max']<1]
+    frame_nocomp_excluded=frame_nocomp_excluded[['MASS37','MASS9000006']]
+    frame_nocomp_excluded=frame_nocomp_excluded.to_numpy()
+ 
+    alpha_shape = alphashape.alphashape(frame_nocomp_excluded, .099)
+    print(alpha_shape)
+    ax.add_patch(PolygonPatch(alpha_shape, alpha=1,ec='pink',fill=False,zorder=20,lw=2,label='False'))
+    
+    plt.scatter(frame_comp['MASS37'],frame_comp['MASS9000006'],c=r_ratio,alpha=1,cmap='jet',vmax=3,vmin=.96,s=45)
+    plt.yscale('log')
+    plt.colorbar(label='$r_{ratio}=r_{True}/r_{False}$')
+    plt.ylabel('$m_{N_{1}}$ (GeV)',fontsize=12)
+    plt.xlabel('$ m_{H^{\\pm}}$ (GeV)',fontsize=12)
+    plt.legend(title='Compression',loc='upper left')
+    
+   
+    
+    
+    
+    plt.title(title)
+    
+    plt.savefig(path_to_plot)
+    plt.close()
+    return
+    
+    
+    
 
 
     
-path_to_frame='/Users/humberto/Documents/work/Scoto-IDM/smodels-dev/smodels/frame_delta5_muons_compression.txt'
+path_to_frame='/Users/humberto/Documents/work/Scoto-IDM/smodels-dev/smodels/frame_5_electrons_compression_3.txt'
 #path_to_frame='/Users/humberto/Documents/work/Scoto-IDM/smodels-dev/smodels/frame_delta5_muons_compression.txt'
-path_to_onlyHSCPframe='/Users/humberto/Documents/work/Scoto-IDM/smodels-dev/smodels/frame_delta5_muons_compression_onlyHSCP.txt'
-title='Fermionic DM, $\\Delta m=5$ GeV, $ H^\pm\\to \\mu^\pm N_1$  '
+path_to_onlyHSCPframe='/Users/humberto/Documents/work/Scoto-IDM/smodels-dev/smodels/frame_5_electrons_compression_onlyHSCP_3.txt'
+title='Fermionic DM, $\\Delta m=5$ GeV, $ H^\pm\\to e^\pm N_1$'
 
-path_to_plot='static_plots/plot_analysis_larger_delta5_electrons_compression.png'
-path_to_plot_topo='static_plots/plot_topo_larger_delta5_muons_compression.png'
-path_to_plot_curves='plots/plot_curves_intersections_delta5_muons.png'
+#path_to_plot='static_plots/plot_analysis_larger_delta5_electrons_compression.png'
+#path_to_plot_topo='static_plots/plot_topo_larger_delta5_muons_compression.png'
+path_to_plot_ctau_curves='plots/plot_curves_ctau_delta5_electrons_compression_3.png'
+path_to_plot_curves='plots/plot_curves_delta5_electrons_compression_3.png'
 frame=openframe(path_to_frame)
 HSCPframe=openframe(path_to_onlyHSCPframe)
 #plotterAnalyses(frame,title,path_to_plot)
 #plotterTopo(frame,title,path_to_plot_topo)
 #plotCurvesTopo(frame,title,path_to_plot_curves)
 plotCurvesTopo2(frame,HSCPframe,title,path_to_plot_curves)
+plotCurvesTopoCtau2(frame,HSCPframe,title,path_to_plot_ctau_curves)
 #plotterTopoHSCPind(frame,title,path_to_plot_topo)
+
+'''
+path_to_frame='/Users/humberto/Documents/work/Scoto-IDM/smodels-dev/smodels/frame_5_electrons_2_compression.txt'
+path_to_frame_nocomp='/Users/humberto/Documents/work/Scoto-IDM/smodels-dev/smodels/frame_5_electrons_2_nocompression.txt'
+frame_comp=openframe(path_to_frame)
+frame_nocomp=openframe(path_to_frame_nocomp)
+title='Fermionic DM, $\\Delta m=5$ GeV, $ H^\pm\\to e^\pm N_1$  '
+path_to_plot='plots/comparing_compression_electrons.png'
+comparing_compression(frame_comp,frame_nocomp,title,path_to_plot)
+
+'''
